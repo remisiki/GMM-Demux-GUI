f928471e5c3ed6265b48da9bb227e8f4c763bdbc
setsunayyw <yangyiwen.sigo@hotmail.com>
2021-12-22 02:09:29 +0800

    First docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 project = 'GMM-Demux'
 copyright = '2021, Hongyi Xin <email@email>'
 author = 'Hongyi Xin'
-html_show_sphinx = False
+# html_show_sphinx = False
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
@@ -35,7 +35,7 @@ extensions = ['myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
